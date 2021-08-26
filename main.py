5b75859735bef03dde077f7b57b11fa5c8bcb72f
michaelka <michael.kalinin.nn@gmail.com>
2021-08-26 12:29:02 +0300

    [local] main + run

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 
 from train import train_num_iter
+
 from utils import get_device
+from utils import seed_everything
 
 from dataset import Image3DDataset
 from model_simple import Simple3DNet
@@ -11,6 +13,8 @@ import pandas as pd
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
+
+# from sklearn.model_selection import train_test_split
 
 # TODO: move
 from torchvision import transforms as T
@@ -24,37 +28,100 @@ def get_train_transform(img_size):
     ])
     return transform
 
-def get_dataset():
+def split_train_df(df, test_size=0.2):
+
+    train_df_all = df.sample(frac=1).reset_index(drop=True)
+
+    all_train_number = len(train_df_all)
+
+    valid_number = int(all_train_number * test_size)
+    train_number = all_train_number - valid_number
+
+    train_df = train_df_all[:train_number]
+    valid_df = train_df_all[train_number:].reset_index(drop=True)
+
+    return train_df, valid_df
+
+def get_dataset(
+    path_to_data,
+    path_to_img,
+    reduce_train,
+    train_number,
+    valid_number,
+):
     IMG_SIZE = 256
-    PATH_TO_DATA = './data/'
-    IMG_PATH_TRAIN = os.path.join(PATH_TO_DATA, 'rsna-brain-tumor-data', 'train')
-    IMG_PATH_TEST = os.path.join(PATH_TO_DATA, 'rsna-brain-tumor-data', 'test')
-
-    train_labels_df = pd.read_csv(PATH_TO_DATA + 'train_labels.csv')
-
-    train_number = 10
-    train_df = train_labels_df.sample(frac=1).reset_index(drop=True).head(train_number)
-
-    train_dataset = Image3DDataset(train_df, IMG_PATH_TRAIN, get_train_transform(IMG_SIZE))
-
-    return train_dataset
+    MAX_DEPTH = 64
     
+    # PATH_TO_DATA = './data/'
+    # IMG_PATH_TRAIN = os.path.join(PATH_TO_DATA, 'rsna-brain-tumor-data', 'train')
+    # IMG_PATH_TEST = os.path.join(PATH_TO_DATA, 'rsna-brain-tumor-data', 'test')
 
-def main():
+    path_to_img_train = os.path.join(path_to_img, 'train')
 
-    print('[main]')
+    train_labels_df = pd.read_csv(os.path.join(path_to_data, 'train_labels.csv'))
+    print(f'[data] Dataset size: {len(train_labels_df)}')
 
-    batch_size = 1
-    learning_rate = 0.001
-    weight_decay = 0
+    print(f'[data] Removing bad data...')
+    train_labels_df = train_labels_df.drop(train_labels_df[train_labels_df['BraTS21ID'] == 109].index)
+    train_labels_df = train_labels_df.drop(train_labels_df[train_labels_df['BraTS21ID'] == 709].index)
+    train_labels_df = train_labels_df.drop(train_labels_df[train_labels_df['BraTS21ID'] == 123].index)
+    print(f'[data] Dataset size: {len(train_labels_df)}')
 
-    train_dataset = get_dataset()
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    model = Simple3DNet()
+    train_df, valid_df = split_train_df(train_labels_df, test_size=0.2)
+    # train_df, valid_df = train_test_split(train_labels_df, test_size=0.2)
+    # train_df = train_df.reset_index(drop=True)
+    # valid_df = valid_df.reset_index(drop=True)
+
+    print(f'[data] Dataset size, train: {len(train_df)}, valid: {len(valid_df)}')
+
+    if reduce_train:
+        train_df = train_df.head(train_number)
+        valid_df = valid_df.head(valid_number)
+        print(f'[data] Reduced dataset size, train: {len(train_df)}, valid: {len(valid_df)}')
+
+    train_dataset = Image3DDataset(train_df, path_to_img_train, MAX_DEPTH, get_train_transform(IMG_SIZE))
+    valid_dataset = Image3DDataset(valid_df, path_to_img_train, MAX_DEPTH, get_train_transform(IMG_SIZE))
+
+    return train_dataset, valid_dataset
+
+#
+# run
+#
+def run(
+    model,
+    device,
+
+    path_to_data='./',
+    path_to_img='./',
+    reduce_train=False,
+    train_number=0,
+    valid_number=0,
+
+    batch_size_train=32,
+    batch_size_valid=32,
+    max_iter=100,
+    valid_iters=[],
+
+    learning_rate=3e-4,
+    weight_decay=1e-3,
+    
+    verbose=True
+):
+
+    print('[run]')
+
+    seed = 2021
+    seed_everything(seed)
+
+    train_dataset, valid_dataset = get_dataset(path_to_data, path_to_img, reduce_train, train_number, valid_number)
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size_train, shuffle=True)
+    valid_loader = DataLoader(valid_dataset, batch_size=batch_size_valid, shuffle=False)
+
+    print(f'[data] DataLoader size, train: {len(train_loader)}, valid: {len(valid_loader)}')
+
     criterion = nn.BCEWithLogitsLoss()
-    device = get_device()
 
-    
     optimizer = torch.optim.SGD(
         model.parameters(),
         lr=learning_rate,
@@ -63,11 +130,67 @@ def main():
         nesterov=True
     )
 
-    max_iter = 10
-    print_every = 1
+    train_info = train_num_iter(
+        model, device,
+        train_loader, valid_loader,
+        criterion, optimizer,
+        max_iter=max_iter,
+        valid_iters=valid_iters,
+        verbose=verbose
+    )
 
-    train_num_iter(model, device, train_loader, criterion, optimizer, max_iter, print_every=print_every)
+    return train_info    
+
+#
+# main
+#
+
+def print_params(params):
+    params_string = (
+        f"\n[params]\n"
+        f"batch_size_train = {params['batch_size_train']}\n"
+        f"batch_size_valid = {params['batch_size_valid']}\n"
+        f"max_iter = {params['max_iter']}\n"
+        f"valid_iters = {params['valid_iters']}\n"
+        f"learning_rate = {params['learning_rate']}\n"
+        f"weight_decay = {params['weight_decay']}\n"
+    )
+    print(params_string)
+
+def main(path_to_data, path_to_img):
+
+    print('[main]')
+
+    device = get_device()
+ 
+    params = dict(
+        # data
+        path_to_data=path_to_data,
+        path_to_img=path_to_img,
+        reduce_train=True,
+        train_number=20,
+        valid_number=10,
+
+        batch_size_train=2,
+        batch_size_valid=2,
+        max_iter=6,
+        valid_iters=[1, 3, 5],
+
+        learning_rate=0.001,
+        weight_decay=0,
+
+        verbose=True
+    )
+
+    print_params(params)
+
+    model = Simple3DNet().to(device)
+
+    return run(model, device, **params)
 
 
-if __name__ == "__main__":   
-    main()
+if __name__ == "__main__":
+    PATH_TO_DATA = './data/'
+    PATH_TO_IMG = os.path.join(PATH_TO_DATA, 'rsna-brain-tumor-data')
+
+    main(PATH_TO_DATA, PATH_TO_IMG)
--- utils.py
+++ utils.py
@@ -7,11 +7,11 @@ import torch
 def get_device():
     if torch.cuda.is_available():       
         device = torch.device("cuda")
-        print('There are %d GPU(s) available.' % torch.cuda.device_count())
-        print('We will use the GPU:', torch.cuda.get_device_name(0))
+        print('[device] There are %d GPU(s) available.' % torch.cuda.device_count())
+        print('[device] We will use the GPU:', torch.cuda.get_device_name(0))
     else:
         device = torch.device("cpu")
-        print('No GPU available, using the CPU instead.')
+        print('[device] No GPU available, using the CPU instead.')
 
     return device
 
